adae57e37d9e8daefc31c95db457125c513d5781
RosarioRu <mariarruval@gmail.com>
2022-07-16 11:56:15 -0700

    Add unfinished constructor to make sure it links to corresponding test.

--- BakeryTracker/Models/Vendor.cs
+++ BakeryTracker/Models/Vendor.cs
@@ -8,11 +8,17 @@ namespace BakeryTracker.Models
   public class Vendor
   {
 
-    public string Name { get; set; };
-    public string Description { get; set; };
-    public int VendorId { get; };
-    public List<Order> VendorOrders { get; set; };
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int VendorId { get; }
+    public List<Order> VendorOrders { get; set; }
     private static List<Vendor> _instancesOfVendors = new List<Vendor> {};
+
+    public Vendor(string nameOfVendor, string descriptionOfVendor)
+    {
+      //constructor for Vendor 
+    }
+
 
     //class should include properties for the vendor's name, a description of the vendor, a List of Orders belonging to the vendor, and any other properties you would like to include.
 
