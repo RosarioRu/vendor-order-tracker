8550f30fcfb89eaaf0763b5555f823089c3afa09
RosarioRu <mariarruval@gmail.com>
2022-07-16 11:18:50 -0700

    Add auto-implementing properties to Vendor class for Name, Description, VendorId and a list of VendorOrders. Add field that is a list of instances of Vendor.

--- BakeryTracker/Models/Vendor.cs
+++ BakeryTracker/Models/Vendor.cs
@@ -1,5 +1,6 @@
+using System.Collections.Generic; 
 using System;
-//using System.Collections.Generic; (for lists)
+
 
 
 namespace BakeryTracker.Models
@@ -7,5 +8,13 @@ namespace BakeryTracker.Models
   public class Vendor
   {
 
+    public string Name { get; set; };
+    public string Description { get; set; };
+    public int VendorId { get; };
+    public List<Order> VendorOrders { get; set; };
+    private static List<Vendor> _instancesOfVendors = new List<Vendor> {};
+
+    //class should include properties for the vendor's name, a description of the vendor, a List of Orders belonging to the vendor, and any other properties you would like to include.
+
   }
-}
+} 
