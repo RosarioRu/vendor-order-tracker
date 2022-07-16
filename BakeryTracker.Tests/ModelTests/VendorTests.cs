7c6977385ce4e7d0d7c8e89a6da66f58490a5cfc
RosarioRu <mariarruval@gmail.com>
2022-07-16 12:01:14 -0700

    Add first test for Vendor class that checks the contructor works - ensure it gives a good fail.

--- BakeryTracker.Tests/ModelTests/VendorTests.cs
+++ BakeryTracker.Tests/ModelTests/VendorTests.cs
@@ -6,12 +6,22 @@ using System;
 namespace BakeryTracker.Tests
 {
   [TestClass]
-  public class VendorTests : IDisposable
+  public class VendorTests
+  // public class VendorTests : IDisposable
   {
 
     //some method to clear lists/teardown so tests don't affect each other
 
     //tests for Vendor class go here
+    [TestMethod]
+    public void VendorConstructor_CreateAnInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Rosie's Cafe", "Coffee shop downtown, typically requests muffins.");
+      // Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+      Assert.AreEqual("Rosie's Cafe", newVendor.Name);
+      // Console.WriteLine(newVendor.Name);
+     
+    }
 
   }
-}
+} 
