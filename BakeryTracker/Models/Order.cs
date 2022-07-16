df38f6471ce5a6358508e2802c3f267c098b9ff0
RosarioRu <mariarruval@gmail.com>
2022-07-16 16:53:50 -0700

    Remove unecessary spacing and comments.

--- BakeryTracker/Models/Order.cs
+++ BakeryTracker/Models/Order.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic; 
 
-
 namespace BakeryTracker.Models
 {
   public class Order
@@ -23,24 +22,20 @@ namespace BakeryTracker.Models
       OrderId = _instancesOfOrders.Count;
     }
 
-    //method to clear the list of orders called _instancesOfOrders
     public static void EmptyOrderList()
     {
       _instancesOfOrders.Clear();
     }
 
-    //method to find instance of Order based on searched for OrderID
     public static Order FindOrder(int orderId)
     {
       return _instancesOfOrders[orderId-1];
     }
-    //method to retrieve list of all instances of Order
+
     public static List<Order> GetAll()
     {
       return _instancesOfOrders;
     }
 
-
-//class should include properties for the title, the description, the price, the date, and any other properties you would like to include.
   }
 } 
--- BakeryTracker/Models/Vendor.cs
+++ BakeryTracker/Models/Vendor.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic; 
 using System;
 
-
-
 namespace BakeryTracker.Models
 {
   public class Vendor
