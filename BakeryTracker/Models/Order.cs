a3337ed3eb5ba9c75cfcdeb2e0039b610fc40f66
RosarioRu <mariarruval@gmail.com>
2022-07-16 14:43:38 -0700

    Add auto-implemented properties, static list of instances of Order, and constructor for Order class.

--- BakeryTracker/Models/Order.cs
+++ BakeryTracker/Models/Order.cs
@@ -6,6 +6,25 @@ namespace BakeryTracker.Models
 {
   public class Order
   {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set}
+    public string Date { get; set}
+    public int OrderId { get; }
+    private static List<Order> _instancesOfOrders = new List<Order> {};
 
+    public Order(string titleOfOrder, string orderDescription, int price, string dateOfOrder)
+    {
+      Title = titleOfOrder;
+      Description = orderDescription;
+      Price = price;
+      Date = dateOfOrder;
+      _instancesOfOrders.Add(this);
+      OrderId = _instancesOfOrders.Count;
+    }
+
+
+
+//class should include properties for the title, the description, the price, the date, and any other properties you would like to include.
   }
-}
+} 
