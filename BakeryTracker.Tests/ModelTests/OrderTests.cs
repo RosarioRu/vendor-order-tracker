9f2c0ba3ee579a8212f5a2e73c55e73a27f81746
RosarioRu <mariarruval@gmail.com>
2022-07-16 00:30:47 -0700

    Add basic using statements and namespace for test file

---
+++ BakeryTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BakeryTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace BakeryTracker.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+
+    //some method to clear lists/teardown so tests don't affect each other
+
+    //tests for Order class will go here
+
+  }
+}
